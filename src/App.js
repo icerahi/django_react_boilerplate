d3462856fdc3f904377eb40e61ba04d65f2794f0
rahi <zanjarwhite@gmail.com>
2021-04-15 13:03:02 +0600

    initialize

---
+++ src/App.js
@@ -0,0 +1,25 @@
+import logo from './rahi.png';
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      <header className="App-header">
+        <img src={logo} className="App-logo" alt="logo" />
+        <p>
+          Start Building Application Using React & Django
+        </p>
+        <a
+          className="App-link"
+          href="https://fb.com/icerahi"
+          target="_blank"
+          rel="noopener noreferrer"
+        >
+          Imran Hasan Rahi
+        </a>
+      </header>
+    </div>
+  );
+}
+
+export default App;
